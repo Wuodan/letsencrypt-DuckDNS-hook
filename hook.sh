30d820e87a0f77dd1a8e2a890750bd7762ee3481
Wuodan <stefan.kuhn.zurich@gmail.com>
2021-01-10 14:47:17 +0100

    Allow token from file to avoid token in env var

--- hook.sh
+++ hook.sh
@@ -8,14 +8,19 @@ set -e
 set -u
 set -o pipefail
 
-if [[ -z "${DUCKDNS_TOKEN}" ]]; then
-  echo " - Unable to locate DuckDNS Token in the environment!  Make sure DUCKDNS_TOKEN environment variable is set"
+printf '\### token is: "%s\n' "$(cat "${DUCKDNS_TOKEN_FILE}")"
+if [[ -z "${DUCKDNS_TOKEN_FILE}" ]] || [[ ! -r "${DUCKDNS_TOKEN_FILE}" ]]; then
+  echo " - Unable to locate DuckDNS Token file in the environment! Make sure DUCKDNS_TOKEN_FILE environment variable is set and file is readable"
+  exit 1
+elif [[ -z "$(cat "${DUCKDNS_TOKEN_FILE}")" ]]; then
+  echo "Unable to read DuckDNS Token from file at ${DUCKDNS_TOKEN_FILE}."
+  exit 1
 fi
 
 deploy_challenge() {
   local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
   echo -n " - Setting TXT record with DuckDNS ${TOKEN_VALUE}"
-  curl "https://www.duckdns.org/update?domains=${DOMAIN}&token=${DUCKDNS_TOKEN}&txt=${TOKEN_VALUE}"
+  curl "https://www.duckdns.org/update?domains=${DOMAIN}&token=$(cat "${DUCKDNS_TOKEN_FILE}")&txt=${TOKEN_VALUE}"
   echo
   echo " - Waiting 30 seconds for DNS to propagate."
   sleep 30
@@ -24,7 +29,7 @@ deploy_challenge() {
 clean_challenge() {
   local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
   echo -n " - Removing TXT record from DuckDNS ${DOMAIN}"
-  curl "https://www.duckdns.org/update?domains=${DOMAIN}&token=${DUCKDNS_TOKEN}&txt=removed&clear=true"
+  curl "https://www.duckdns.org/update?domains=${DOMAIN}&token=$(cat "${DUCKDNS_TOKEN_FILE}")&txt=removed&clear=true"
   echo
 }
 
